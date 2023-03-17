a91d274399addcb6ff8eb0ea3525239817a7cdce
Harold Martin <harold.martin@gmail.com>
2023-03-17 06:57:18 -0700

    run build in workflow

--- webpack.common.js
+++ webpack.common.js
@@ -15,21 +15,6 @@ module.exports = {
         exclude: /node_modules/,
         test: /\.tsx?$/,
         use: "ts-loader"
-      },
-      {
-        exclude: /node_modules/,
-        test: /\.scss$/,
-        use: [
-          {
-            loader: "style-loader" // Creates style nodes from JS strings
-          },
-          {
-            loader: "css-loader" // Translates CSS into CommonJS
-          },
-          {
-            loader: "sass-loader" // Compiles Sass to CSS
-          }
-        ]
       }
     ]
   },
